d28901548c051284346dec62c46aa40a41e07af0
Michelle <mlbagell@gmail.com>
2017-02-14 20:59:16 -0500

    updated style and fixed aside on blog page

--- wp-content/themes/accelerate-theme/index.php
+++ wp-content/themes/accelerate-theme/index.php
@@ -19,23 +19,25 @@ get_header(); ?>
 	<section class="blog-page">
 		<div class="site-content">
 			<div class="main-content">
-				
-			<?php
-			if ( have_posts() ) :
-				// Start the Loop.
-				while ( have_posts() ) : the_post(); ?>
-				
+				<div class="blog-post-entry">
+					<?php
+					if ( have_posts() ) :
+						// Start the Loop.
+						while ( have_posts() ) : the_post(); ?>
+
 					<article class="post-entry">
 						<div class="entry-wrap">
 							<header class="entry-header">
 								<div class="entry-meta">
 									<time class="entry-time"><?php the_date();?></time>
 								</div>
-								<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									<h2 class="entry-title">
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+									</h2>
 							</header>
-						<div class="entry-summary">
-								<?php the_excerpt(); ?>
-						</div>
+							<div class="entry-summary">
+									<?php the_excerpt(); ?>
+							</div>
 							<footer class="entry-footer">
 								<div class="entry-meta">
 									<span class="entry-terms author">Written by <?php the_author_posts_link(); ?></span>
@@ -44,24 +46,24 @@ get_header(); ?>
 								</div>
 							</footer>
 						</div>
-					
+
 					</article>
-					
-			<?php endwhile; endif; ?>
+					<?php endwhile; endif; ?>
+				</div>
+				<div class="sidebarx">
+					<?php get_sidebar(); ?>
+				</div>
 			</div>
-
-			<?php get_sidebar(); ?>
-		
-				<div class="clearfix"></div>
-					
-			<div id="navigation" class="navigation"> 
-				<div class="left"><?php next_posts_link('&larr; <span>Older Posts</span>'); ?></div>
-				<div class="right"><?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?></div>
+			<div class="clearfix"></div>
+			<div id="navigation" class="navigation">
+				<div class="left">
+					<?php next_posts_link('&larr; <span>Older Posts</span>'); ?>
+				</div>
+				<div class="right">
+					<?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?>
+				</div>
 			</div>
-
-						
 		</div>
-
 	</section>
 	<!-- END blog page -->
 <?php get_footer();
